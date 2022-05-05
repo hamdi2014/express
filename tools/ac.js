6c3947a57bed9e258dc356edc974718c9273b7f3
hamdi2014 <cn120.2010@gmail.com>
2022-05-06 03:09:55 +0430

    acces controll

--- tools/ac.js
+++ tools/ac.js
@@ -7,4 +7,16 @@ accessController.checkAdminRoleMiddleWare=(req,res,next)=>{
     return res.sendStatus(403)
 }
 
+accessController.checkUserRoleAccessForDeleteAndUpdateUserData=(req,res,next)=>{
+    if(req.user.role!=='admin' && req.params.id==="me"){
+        res.locals.userId=req.user._id
+        return next()
+    }
+    if(req.user.role==='admin'){
+        res.locals.userId=req.params.id
+        return next();
+    }
+    return res.sendStatus(403)
+}
+
 module.exports=accessController;
